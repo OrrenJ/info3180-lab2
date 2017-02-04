2af5365085e7d0a37baa9039972bf2b0735692b4
Orren Joseph <ojey11@gmail.com>
2017-02-03 19:58:55 -0500

    timeinfo

--- app/views.py
+++ app/views.py
@@ -7,7 +7,14 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for
+import time
 
+###
+# Time function
+###
+
+def timeinfo():
+	return time.strftime("%a, %d %b %Y")
 
 ###
 # Routing for your application.
@@ -22,8 +29,11 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Orren Joseph")
 
+@app.route('/profile/')
+def profile():
+	return render_template('profile.html', date=timeinfo())
 
 ###
 # The functions below should be applicable to all Flask apps.
